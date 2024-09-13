76f0e2a349c39330dbfd67d560e7f706b2fbaf69
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-09-13 13:23:11 +0200

    Add function to fetch products from API

--- app/page.js
+++ app/page.js
@@ -1,6 +1,26 @@
-import Image from "next/image";
+import ProductCard from "../components/ProductCard";
+import Pagination from "../components/Pagination";
+import ProductCarousel from "@/components/ProductCarousel";
 
-export default function Home() {
+/**
+ * Fetches products from the API.
+ * 
+ * @async
+ * @function fetchProducts
+ * @param {number} [page=1] - The current page number to fetch products for.
+ * @returns {Promise<Array>} The list of products for the current page.
+ * @throws {Error} If the fetch request fails.
+ */
+async function fetchProducts(page = 1) {
+  const res = await fetch(`https://next-ecommerce-api.vercel.app/products?skip=${(page - 1) * 20}&limit=20`, {
+    cache: 'no-store', // Ensure data is always fresh
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch products');
+  }
+  return res.json();
+}
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
